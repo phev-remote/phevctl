54b2817223836960e434e59087a9cb9fb5067db4
Jamie Nuttall <jamie@wattu.com>
2018-12-09 17:11:57 +0000

    removed redundanct code

--- src/phev_register.c
+++ src/phev_register.c
@@ -13,7 +13,7 @@ phevRegisterCtx_t * phev_register_init(phevRegisterSettings_t settings)
 
     ctx->pipe = settings.pipe;
     ctx->complete = settings.complete;
-    
+
     memcpy(ctx->mac,settings.mac,MAC_ADDR_SIZE);
 
     ctx->pipe->ctx = ctx;
@@ -22,16 +22,6 @@ phevRegisterCtx_t * phev_register_init(phevRegisterSettings_t settings)
 
     return ctx;
 }
-void phev_register_sendRegister(phev_pipe_ctx_t * ctx)
-{
-    phevMessage_t * initMessage = phev_core_simpleRequestCommandMessage(KO_WF_INIT_RQ_SP,0);
-    message_t * message = phev_core_convertToMessage(initMessage);
-
-    msg_pipe_outboundPublish(ctx->pipe, message);
-    free(message);
-    free(initMessage);
-}
-
 void phev_register_sendMac(phev_pipe_ctx_t * ctx)
 {
     LOG_V(TAG,"START - sendMac");
@@ -42,7 +32,6 @@ void phev_register_sendMac(phev_pipe_ctx_t * ctx)
     LOG_V(TAG,"END - sendMac");
     
 }
-
 int phev_register_eventHandler(phev_pipe_ctx_t * ctx, phevPipeEvent_t * event)
 {
     switch(event->event) 
